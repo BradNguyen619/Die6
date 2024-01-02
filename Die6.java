075b7afd857678ba000b0ba3debd94a5fcd4177d
Brad <143217357+BradNguyen619@users.noreply.github.com>
2024-01-02 11:04:11 -0800

    Update Die6.java

--- Die6.java
+++ Die6.java
@@ -1,29 +1,24 @@
 
 public class Die6
-{
+{ 
   
   private int value;
 
- 
   public Die6() {
     this.roll();
   }
 
- 
   public int getValue() {
     return value;
   }
 
- 
   public void roll() {
     this.value = (int)(Math.random() * 6) + 1;
   }
 
-  
   public int rollAndGetValue() {
     roll();
     return getValue();
   }
-
 }
 
